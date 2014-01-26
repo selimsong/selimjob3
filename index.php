1fa5904df7704aa561fde0deae25f7ee4760819b
selim song <selim@e.com>
2014-01-26 15:22:43 +0800

    'm'

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
 <div id="pagewrap">
 	<div id="content">
-		<img class="gua_bj01" src="./images/bg.jpg"  />
+		<img  name="bg" src="./images/bg.jpg"  />
 		<div class="gua_area" id="wScratchPad" ></div>
         <a href="javascript:reset_to();"><img id="try_again" class="tip" src="./images/tip01.png" ></a>
         <a href="#"><img id="update_info"  class="tip" src="./images/tip02.png" ></a>
@@ -24,9 +24,9 @@
            亲爱的活动，我们要开 
            亲爱的活动，我们要开 
         </div>
-        <div class="detail" ><a href="http://www.baidu.com"><img class="gua_bj01" src="detail.png" /></a></div>
-        <div class="user" ><img class="gua_bj01" src="user.png" /></div>
-        <div class="win" ><img class="gua_bj01" src="win.png" /></div>
+        <div class="detail" ><a href="http://www.baidu.com"><img  name="detail"  src="./images/detail.png" /></a></div>
+        <div class="user" ><img  name="user"  src="./images/user.png" /></div>
+        <div class="win" ><img  name="win"  src="./images/win.png" /></div>
 	</div>
 </div>
 </body>
