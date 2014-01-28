40e86d5dda101f9c5a7a507430bf63df41adb97f
selim song <selim@e.com>
2014-01-28 14:10:49 +0800

    'm'

--- dingyue.php
+++ dingyue.php
@@ -9,13 +9,12 @@ $msgId        = $xml->MsgId;
 //subscribe event
 if ($msgType == 'event') {
 	if ($xml->Event == 'subscribe') {
-			 $description = "新年到了，小伙伴们是不是按捺不住要给大伙儿发祝福呢？最酷莫过于亲手定制一张贺卡，即刻在对话框中上传一张祝福对象的照片（竖版或正方形），获取你为TA私人定制的新年贺卡。";
-	        replyTextAndImg($sendUserId, $developerId, '#心意自造#潮卡送到', $description, 'http://115.29.49.54/activity.jpg?v=123', 'http://tongyi.mei94.com/page?v=1234');
+	     replyDoubleTextAndImg($toUserName, $fromUserName)
 	}
 	exit();
 }
 
-function replyDoubleTextAndImg($toUserName, $fromUserName,){
+function replyDoubleTextAndImg($toUserName, $fromUserName){
 
       $textTpl = "<xml>
 	                <ToUserName><![CDATA[%s]]></ToUserName>
@@ -26,7 +25,7 @@ function replyDoubleTextAndImg($toUserName, $fromUserName,){
                     <Articles>
 					<item>
 					<Title><![CDATA[大家好]]></Title> 
-					<Description><![CDATA[大家好2]]></Description>
+					<Description><![CDATA[大家好2描述]]></Description>
 					<PicUrl><![CDATA[http://115.29.49.54/eventimg/sc.jpg]]></PicUrl>
 					<Url><![CDATA[http://115.29.49.54/scratch/]]></Url>
 					</item>
@@ -37,7 +36,7 @@ function replyDoubleTextAndImg($toUserName, $fromUserName,){
 					</item>
 					</Articles>
                </xml>";			
-	$resultStr = sprintf($textTpl, $toUserName, $fromUserName, time(),  $title, $description, $picUrl, $url,$title, '我们飞啦', '', '');
+	$resultStr = sprintf($textTpl, $toUserName, $fromUserName, time());
   }
 	echo $resultStr;
 }
