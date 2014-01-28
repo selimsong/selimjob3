b547bfc151152139439b2c750093979626ef6e0a
selim song <selim@e.com>
2014-01-28 13:12:03 +0800

    'm'

--- data.php
+++ data.php
@@ -4,7 +4,7 @@ $con = mysql_connect("localhost","root","blabla1234");
 mysql_select_db("scra", $con);
 
 mysql_query("INSERT INTO users (name, phone, email, address, wintype, ipaddress, createdate, createtime) 
-VALUES ('".$_POST['name']."', '".$_POST['phone']."', '".$_POST['email']."', '".$_POST['address']."', NULL,
-NULL, ".date(d).", ".time().")") or die(mysql_error());
+VALUES ('".$_POST['name']."', '".$_POST['phone']."', '".$_POST['email']."', '".$_POST['address']."', 1,
+2, ".date(d).", ".time().")") or die(mysql_error());
 
 mysql_close($con);
