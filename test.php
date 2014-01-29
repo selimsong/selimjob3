aa9eedf8f601d66e5d44d25f08c4c6b0f8e8a129
selim song <selim@e.com>
2014-01-29 21:08:36 +0800

    'm'

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
 <?php
 
 file_put_contents('a.txt', json_encode($_GET), FILE_APPEND);
-file_put_contents('ab.txt',json_encode($_POST), FILE_APPEND);
+file_put_contents('b.txt',json_encode($_POST), FILE_APPEND);
