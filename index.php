927a4c780e6c452f92c1f66cae4c978156b410c4
selim song <selim@e.com>
2014-01-30 10:50:00 +0800

    'm'

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ foreach($img_array as $k => $value){
         <div id="handwrapper"><img src="hand.png" /></div>
     </div>
     
-    <script type="text/javascript" src="jquery.min.js"></script>
+    <script type="text/javascript" src="jquery-1.7.2.min.js"></script>
     <script type="text/javascript" src="jquery.mobile-1.2.0.min.js"></script>
     <script type="text/javascript" src="wScratchPad.js"></script>
     <script>
