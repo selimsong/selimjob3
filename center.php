021a326a271b94d2380f42d3ee835204418568d6
selim song <selim@e.com>
2014-01-30 15:55:32 +0800

    'm'

--- center.php
+++ center.php
@@ -32,7 +32,7 @@
         <a id="winline" name="winline"> </a>
       <div class="luck_name_box" >
         <div class="list01" >
-                <h3>艾薇儿签名潮T获得者：</h3>
+                <h3>艾薇儿亲笔签名潮T获得者：</h3>
                   <div style="overflow:hidden;min-height:160px;position: relative;   " id="demo">
                     <div style="position: absolute;left:0%;height: 160px;" class="feiji" id="demo1 ">
                             <p class="list_jipiao"><em class="praiseNo01 dat-UserName">徐*</em>
@@ -72,7 +72,7 @@
         <div class="list01">
                 <h3>艾薇儿潮酷水杯获得者：</h3>
                   <div style="overflow:hidden;min-height:160px;position: relative;   " id="demo">
-                    <div style="position: absolute;left:5%;height: 160px;" class="shuibei" id="demo1 ">
+                    <div style="position: absolute;left:0%;height: 160px;" class="shuibei" id="demo1 ">
                             <p class="list_jipiao"><em class="praiseNo01 dat-UserName">徐*</em>
                                 <em  class="praiseNo02 dat-Phone">138***37676</em>
                                </p>
@@ -105,7 +105,27 @@
 
 
 $(".tijaio02").click(function () {
- 
+   if('' == $(".name").val()){
+	   alert("请输入您的姓名");
+	   return false;
+	}
+   
+    if('' == $(".tel").val()){
+	   alert("请输入您的电话号码");
+	   return false;
+	}
+	
+	if('' == $(".email").val()){
+	   alert("请输入您的Email");
+	   return false;
+	}
+	
+		
+	if('' == $(".address").val()){
+	   alert("请输入您的邮寄地址");
+	   return false;
+	}
+   
    $.post( "data.php", { name: $(".name").val(), phone: $(".tel").val(), email: $(".email").val(), address: $(".address").val() })
      .done(function( data ) {
            alert("提交信息成功！");
