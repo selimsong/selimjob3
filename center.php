f81d48b420c313b7b53060e5a2f030ba8ebc803e
selim song <selim@e.com>
2014-01-30 15:35:15 +0800

    'm'

--- center.php
+++ center.php
@@ -70,9 +70,9 @@
        
       <div class="luck_name_box2">
         <div class="list01">
-                <h3>艾薇儿签名潮T获得者：</h3>
+                <h3>艾薇儿潮酷水杯获得者：</h3>
                   <div style="overflow:hidden;min-height:160px;position: relative;   " id="demo">
-                    <div style="position: absolute;left:5%;height: 160px;" class="feiji" id="demo1 ">
+                    <div style="position: absolute;left:5%;height: 160px;" class="shuibei" id="demo1 ">
                             <p class="list_jipiao"><em class="praiseNo01 dat-UserName">徐*</em>
                                 <em  class="praiseNo02 dat-Phone">138***37676</em>
                                </p>
@@ -87,20 +87,7 @@
                                </p>
                     </div>
                     
-                    <div style="position: absolute;left:52%;height: 160px;" class="feiji">
-                            <p class="list_jipiao"><em class="praiseNo01 dat-UserName">徐*</em>
-                                <em id="phone1" class="praiseNo02 dat-Phone">138***37676</em>
-                               </p>
-                                <p class="list_jipiao"><em class="praiseNo01 dat-UserName">张*</em>
-                                <em id="phone1" class="praiseNo02 dat-Phone">136***53663</em>
-                                  </p>
-                                <p class="list_jipiao"><em class="praiseNo01 dat-UserName">王*</em>
-                                <em id="phone1" class="praiseNo02 dat-Phone">136***32328</em>
-                               </p>
-                                                   <p class="list_jipiao"><em class="praiseNo01 dat-UserName">李*</em>
-                                <em id="phone1" class="praiseNo02 dat-Phone">136***32328</em>
-                               </p>
-                    </div>
+              
 
                 </div>
             </div>
@@ -128,8 +115,33 @@ $(".tijaio02").click(function () {
 
 
 });
+  function goToMarquee(selector) {
+            var speed = 50;
+            var ele = $(selector);
+            var MyMar = "";
+           
+            
+            var h = $(selector).parent().height();
+            if (ele.eq(0).height() < h) { return; }
+            if (h < ele.eq(0).height()) { h = ele.eq(0).height(); }
+            ele.eq(1).css("top", h + "px");
+            ele.eq(1).html(ele.eq(0).html());
+            
+            function Marquee() {
+                ele.css({ "top": "-=1px" });  
+                var top1 = Math.abs(ele.eq(0).position().top);
+                var top2 = Math.abs(ele.eq(1).position().top);
+                if (top1 > h) { ele.eq(0).css("top", h + "px");  }
+                if (top2 > h) { ele.eq(1).css("top", h + "px"); }
+            }
+            MyMar = setInterval(function () { Marquee(); }, speed);
+            demo.ontouchstart = function () { clearInterval(MyMar) };
+            demo.ontouchend = function () { MyMar = setInterval(function () { Marquee(); }, speed); };
+        }
 
-
+	goToMarquee(".shuibei");
 </script>
+
+
 </body>
 </html>
