cf28c3c94d62e44c9c63617cec37092f66a0fafa
selim song <selim@e.com>
2014-01-31 16:10:08 +0800

    'm'

--- tcms/application/backend/config/config.php
+++ tcms/application/backend/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/selimjob3/tcms/';
+$config['base_url']	= 'http://115.29.49.54/scratch/tcms/';
 
 /*
 |--------------------------------------------------------------------------
