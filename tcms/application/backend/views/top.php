afd66a9ef179e45c2faf20ce157e9ed882033f99
selim song <selim@e.com>
2014-01-31 16:49:02 +0800

    'm'

--- tcms/application/backend/views/top.php
+++ tcms/application/backend/views/top.php
@@ -3,7 +3,7 @@ $lang = $this->session->userdata('default_lang');
 ?>
 <div class="mainhd">
 	<div class="uinfo">
-		<p>您好, <em><?php echo $this->session->userdata('UserName'); ?></em> [ <a href="<?php echo  base_url();  ?>admin/login/logout" target="_top">退出</a> ]</p>
+		<p>您好, <em><?php echo $this->session->userdata('UserName'); ?></em> [ <a href="<?php echo  base_url();  ?>admin/index.php?/login/logout" target="_top">退出</a> ]</p>
 					
 			</div>
 </div>
