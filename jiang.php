a0a27dd875a48724a86cecea0260772a096f23ef
selim song <selim@e.com>
2014-01-31 10:52:02 +0800

    'm'

--- jiang.php
+++ jiang.php
@@ -3,7 +3,7 @@ $shuibei = array(30=>10, 31=>7, 1=>7, 2=>7);
 $shirt   =1;
 $tfile = './jiang/t.txt';
 $date = date('d');
-$randA = rand(1, 100);
+$randA = rand(1, 200);
 $rate = 3;
 if($randA> 21 && $randA<30 ){
 for($i=1; $i<=$shuibei[$date]; $i++){
