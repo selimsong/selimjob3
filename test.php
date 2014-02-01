f31649997a5c086e9f2927f9c5ac45bfbf42dfc9
selim song <selim@e.com>
2014-02-01 10:06:29 +0800

    'm'

--- test.php
+++ test.php
@@ -1,36 +1,3 @@
 <?php
-header("Content-type: text/html; charset=utf-8");
-$con = mysql_connect("localhost","root","blabla1234");
-mysql_select_db("scra", $con);
-
-$win1_key = base64_encode('wia'.date('l'));
-$win2_key = base64_encode('wib'.date('l'));
-
-
-
-$result = mysql_query("SELECT * FROM users");
-
-while($row = mysql_fetch_array($result))
- {
-  $winType =  $row['wintype'];
-  $dtime = $row['createtime'];
-  
-  $d_a  = 'wia'.date('l', $dtime);
-  $d_b  = 'wib'.date('l', $dtime);
-  echo $winType, '------',$d_b;
-  if(md5($d_a) == $winType){
-      var_dump($row);
-      var_dump($row['wintype']);
-	  echo 'a';
-   }
-    
-   if(md5($d_b) == $winType){
-      var_dump($row['id']);
-      var_dump($row['wintype']);
-	  echo 'b';
-   }
-
-    echo "<br /><br /><br />";
-  }
-
-mysql_close($con);
+$file = './jiang/1shui.txt";
+unlink($file);
