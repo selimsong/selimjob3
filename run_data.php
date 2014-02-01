e722342c3dfb5c2c42094cd34b1367e765c2eb93
selim song <selim@e.com>
2014-02-01 10:43:24 +0800

    'm'

--- run_data.php
+++ run_data.php
@@ -4,15 +4,29 @@ header("Content-type: text/html; charset=utf-8");
 $con = mysql_connect("localhost","root","blabla1234");
 mysql_select_db("scra", $con);
 
-$result = mysql_query("SELECT * FROM users");
-
+$result = mysql_query("SELECT * FROM users order by createtime desc");
+echo "获水杯";
 while($row = mysql_fetch_array($result))
   {
-   echo '<p class="list_jipiao"><em class="praiseNo01 dat-UserName">'.utf8Substr($row['name'],0,1).'*</em><em  class="praiseNo02 dat-Phone">'.substr($row['phone'],0,3).'***'.substr($row['phone'],-5).'</em></p>';
+    $type = substr(base64_decode($row['wintype']), 0,3);
+     if('wia' == $type){
+		   echo '<p class="list_jipiao"><em class="praiseNo01 dat-UserName">'.utf8Substr($row['name'],0,1).'*</em><em  class="praiseNo02 dat-Phone">'.substr($row['phone'],0,3).'***'.substr($row['phone'],-5).'</em></p>';           
+       }
 
-   var_dump($row);
-   echo "<br />";
-  }
+}
+
+echo '获T';
+echo "<br />br />br />";
+while($row = mysql_fetch_array($result))
+  {
+    $type = substr(base64_decode($row['wintype']), 0,3);
+     if('wib' == $type){
+		   echo '<p class="list_jipiao"><em class="praiseNo01 dat-UserName">'.utf8Substr($row['name'],0,1).'*</em><em  class="praiseNo02 dat-Phone">'.substr($row['phone'],0,3).'***'.substr($row['phone'],-5).'</em></p>';        
+       }
+
+
+}
+
 
 mysql_close($con);
 
