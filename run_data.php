2bf3766b228c85607774d9e48f30a7518775bd3e
selim song <selim@e.com>
2014-02-01 10:48:47 +0800

    'm'

--- run_data.php
+++ run_data.php
@@ -6,26 +6,30 @@ mysql_select_db("scra", $con);
 
 $result = mysql_query("SELECT * FROM users order by createtime desc");
 echo "获水杯";
+$shuibei = array();
+$tshirt = array();
 while($row = mysql_fetch_array($result))
   {
     $type = substr(base64_decode($row['wintype']), 0,3);
      if('wia' == $type){
-		   echo '<p class="list_jipiao"><em class="praiseNo01 dat-UserName">'.utf8Substr($row['name'],0,1).'*</em><em  class="praiseNo02 dat-Phone">'.substr($row['phone'],0,3).'***'.substr($row['phone'],-5).'</em></p>';           
+        $shuibei[] = array('name'=>utf8Substr($row['name'],0,1), 'phone' => substr($row['phone'],0,3).'***'.substr($row['phone'],-5));      
        }
+      if('wib' == $type){
+           $tshirt[] = array('name'=>utf8Substr($row['name'],0,1), 'phone' => substr($row['phone'],0,3).'***'.substr($row['phone'],-5));   
+       }
+}
 
+froeach($shuibei as $value){
+  echo '<p class="list_jipiao"><em class="praiseNo01 dat-UserName">'$value['name'].'*</em><em  class="praiseNo02 dat-Phone">'.$value['phone'].'</em></p>';   
 }
 
 echo '获T';
 echo "<br />br />br />";
-while($row = mysql_fetch_array($result))
-  {
-    $type = substr(base64_decode($row['wintype']), 0,3);
-     if('wib' == $type){
-		   echo '<p class="list_jipiao"><em class="praiseNo01 dat-UserName">'.utf8Substr($row['name'],0,1).'*</em><em  class="praiseNo02 dat-Phone">'.substr($row['phone'],0,3).'***'.substr($row['phone'],-5).'</em></p>';        
-       }
 
-
+froeach($tshirt as $value){
+  echo '<p class="list_jipiao"><em class="praiseNo01 dat-UserName">'$value['name'].'*</em><em  class="praiseNo02 dat-Phone">'.$value['phone'].'</em></p>';   
 }
+
 
 
 mysql_close($con);
