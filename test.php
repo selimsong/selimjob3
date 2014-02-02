e523be04fc15a3bef762e19c159cc9ea9ac5a8b3
selim song <selim@e.com>
2014-02-02 14:52:11 +0800

    'm'

--- test.php
+++ test.php
@@ -1,3 +1,19 @@
 <?php
-$file = './jiang/1shui.txt';
-unlink($file);
+$shuibei = array('30'=>10, '31'=>11, '01'=>1, '02'=>7);
+$shirt   =1;
+$tfile = './jiang/t.txt';
+$date = date('d');
+$randA = rand(1, 80);
+$rate = 3;
+
+for($i=1; $i<=$shuibei[$date]; $i++){
+	$file = './jiang/'.$i."shui.txt";
+	if(file_exists($file)){
+	  $rate = 1;
+	  //unlink($file);
+	  break;
+	}
+}
+
+echo $rate;
+
