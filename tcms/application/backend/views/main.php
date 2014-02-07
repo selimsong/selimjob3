1146cdf7a5c0c6286f75dc92e148a576f128c2c9
selim song <selim@e.com>
2014-02-07 09:17:30 +0800

    'm'

--- tcms/application/backend/views/main.php
+++ tcms/application/backend/views/main.php
@@ -1,18 +1,11 @@
 <div class="container">
-	<h3> 统计信息</h3>
-	<ul class="memlist fixwidth">
-		<li><em><a href="admin.php?m=user&a=ls">用户总数</a>:</em>12</li>
-	</ul>
-	
-	
 	<h3>系统信息</h3>
 	<ul class="memlist fixwidth">
 		
-		<li><em>服务器软件:</em>Apache/2.4.4 (Win32) OpenSSL/0.9.8y PHP/5.4.16</li>
+		<li><em>服务器软件:</em>Apache</li>
 		<li><em>MySQL 版本:</em>5.5.32</li>
-		<li><em>上传许可:</em>2M</li>
+		<li><em>上传许可:</em>4M</li>
 		<li><em>当前数据库尺寸:</em>395.61 KB</li>		
-			
 	</ul>
 	<h3>开发团队</h3>
 	<ul class="memlist fixwidth">
