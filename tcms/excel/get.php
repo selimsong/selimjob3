2f51d62504506630c50c5d8684c49598608589e3
selim song <selim@e.com>
2014-02-09 10:11:02 +0800

    'm'

--- tcms/excel/get.php
+++ tcms/excel/get.php
@@ -15,8 +15,7 @@ $objPHPExcel = $objReader->load("win.xls");
 
 $con = mysql_connect("localhost","root","blabla1234");
 mysql_select_db("scra", $con);
-$result = mysql_query("SELECT * FROM users order by createtime desc");
-
+$result = mysql_query("SELECT * FROM users where wintype !=1 and wintype !=2");
 $user = array();
 while($row = mysql_fetch_array($result))
 {
@@ -24,19 +23,27 @@ $user[] = $row;
 }
 mysql_close($con);
 
-
 $objPHPExcel->getActiveSheet()->setCellValue('D1', PHPExcel_Shared_Date::PHPToExcel(time()));
-
 $baseRow = 5;
 foreach($user as $r => $dataRow) {
+	$winT= null;
+	$type = substr(base64_decode($dataRow['wintype']), 0,3);
+	if('wia' == $type){
+		$winT ="水杯";
+	}elseif ('wib' == $type){
+		$winT ='T';
+	}
 	$row = $baseRow + $r;
 	$objPHPExcel->getActiveSheet()->insertNewRowBefore($row,1);
 
 	$objPHPExcel->getActiveSheet()->setCellValue('A'.$row, $r+1)
 	                              ->setCellValue('B'.$row, $dataRow['name'])
-	                              ->setCellValue('C'.$row, $dataRow['name'])
+	                              ->setCellValue('C'.$row, $winT)
 	                              ->setCellValue('D'.$row, $dataRow['phone'])
-	                              ->setCellValue('E'.$row, $dataRow['email']);
+	                              ->setCellValue('E'.$row, $dataRow['email'])
+	                              ->setCellValue('F'.$row, $dataRow['address'])
+	                              ->setCellValue('G'.$row, date("Y-m-d H:i:s",$dataRow['createtime']))
+	                              ->setCellValue('H'.$row, $dataRow['ipaddress']);
 }
 
 $objPHPExcel->getActiveSheet()->removeRow($baseRow-1,1);
