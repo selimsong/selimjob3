b2bb50f11d621431e709119056a40d870feaf176
selim song <selim@e.com>
2014-02-12 17:40:34 +0800

    'm'

--- jiang.php
+++ jiang.php
@@ -3,9 +3,9 @@ $shuibei = array('10'=>7, '11'=>4, '12'=>4, '13'=>4, '14'=>4, '15'=>4, '16'=>4, 
 $shirt   =1;
 $tfile = './jiang/t.txt';
 $date = date('d');
-$randA = rand(1, 400);
+$randA = rand(1, 300);
 $rate = 3;
-if($randA> 21 && $randA<30 ){
+if($randA> 21 && $randA<40 ){
 for($i=1; $i<=$shuibei[$date]; $i++){
 	$file = './jiang/'.$i."shui.txt";
 	if(file_exists($file)){
