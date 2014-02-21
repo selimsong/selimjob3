8e4e79f1e76fdf395b2d45f819eda7bc8762122d
selim song <selim@e.com>
2014-02-21 18:49:04 +0800

    'm'

--- jiang.php
+++ jiang.php
@@ -1,9 +1,9 @@
 <?php
 $shirt   =1;
 //$tfile = './jiang/t.txt';
-$randA = rand(1, 600);
+$randA = rand(1, 400);
 $rate = 3;
-if($randA> 21 && $randA<30){
+if($randA> 21 && $randA<40){
 for($i=1; $i<=5; $i++){
 	$file = './jiang/'.$i."shui.txt";
 	if(file_exists($file)){
